760c7987010f94ed60c94ecdb44db4251e5f4b06
Williams <widorenyin0@gmail.com>
2025-05-11 02:16:42 +0100

    Add Tailwind CSS configuration and update build manifests
    - Introduced a new Tailwind CSS configuration file to manage styles and themes.
    - Updated build manifests to reflect changes in the application structure, removing unused routes and files.
    - Cleaned up development files in the build manifest for improved performance.
    - Adjusted webpack cache files for both client and server environments.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -190,7 +190,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("566a630ef5df7a75")
+/******/ 		__webpack_require__.h = () => ("73d585184a14f546")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
@@ -1401,11 +1401,6 @@
 /******/ 		var chunkLoadingGlobal = self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || [];
 /******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/nonce */
-/******/ 	(() => {
-/******/ 		__webpack_require__.nc = undefined;
 /******/ 	})();
 /******/ 	
 /************************************************************************/
